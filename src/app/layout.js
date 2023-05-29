77f7d8f42a34487a222cc47b10eac00d27bd38f5
Muhammad Ahmer <85364308+ahmeryangster@users.noreply.github.com>
2023-05-29 19:11:45 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+          <head>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+      </head>
       <body className={roboto.className}>
         <Header/>
         {children}</body>
